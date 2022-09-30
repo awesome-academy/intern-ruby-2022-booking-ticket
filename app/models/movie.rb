f71692f80fc31c6b08bfc38ecdb31fa1035a0005
Cong Vu <vuquoccong01@gmail.com>
2022-09-30 09:33:56 +0700

    Fix admin manage movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,27 @@
 class Movie < ApplicationRecord
-  MOVIE_ATTRS = %i(title description rating img_link
+  MOVIE_ATTRS = %i(title description rating image
                    duration_min release_time language
-                   director cast age_range category_id).freeze
+                   director cast age_range).freeze
 
   has_many :comments, dependent: :destroy
   has_many :show_times, dependent: :restrict_with_exception
-  belongs_to :category
+  has_many :movie_categories, dependent: :destroy
+  has_many :categories, through: :movie_categories
+
+  accepts_nested_attributes_for :movie_categories
+
+  has_one_attached :image
 
   validates :title, presence: true
   validates :release_time, presence: true
+  validates :image, content_type: {in: %w(image/jpeg image/gif image/png),
+                                   message: "must be a valid image format"},
+                    size:         {less_than: 5.megabytes,
+                                   message: "should be less than 5MB"}
 
-  delegate :name, to: :category
   scope :sort_list, ->{order :release_time}
+
+  def display_image
+    image.variant(resize_to_limit: [1000, 600])
+  end
 end
